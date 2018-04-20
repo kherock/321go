0783db5067bd28cb94cce82b7379b9f87c70e68b
Kyle Herock <kherock@gmail.com>
2018-04-20 06:21:09 -0400

    feat(Room): Share full room state with client on synchronize
    This includes recording the time of PLAYING events so that the server can calculate the expected player time when synchronizing other clients.

--- src/models/room.js
+++ src/models/room.js
@@ -1,5 +1,9 @@
 const rooms = new Map();
 
+const kClients = Symbol('clients');
+const kPlayerTime = Symbol('playerTime');
+const kServerTime = Symbol('serverTime');
+
 export default class Room {
   static get(id) {
     return rooms.get(id);
@@ -15,21 +19,27 @@ export default class Room {
 
   constructor(id) {
     this.id = id;
-    this.clients = new Set();
+    this[kClients] = new Set();
     rooms.set(id, this);
   }
 
   get users() {
-    return this.clients.size;
+    return this[kClients].size;
+  }
+
+  get currentTime() {
+    return this[kServerTime] === undefined
+      ? this[kPlayerTime]
+      : this[kPlayerTime] + (Date.now() - this[kServerTime]) / 1000;
   }
 
   join(socket) {
-    this.clients.add(socket);
+    this[kClients].add(socket);
     socket.send(JSON.stringify({ type: 'SYNCHRONIZE', ...this.toJSON() }));
   }
 
   leave(socket) {
-    this.clients.delete(socket);
+    this[kClients].delete(socket);
     if (!this.users) {
       rooms.delete(this.id);
       console.log('room ' + this.id + ' destroyed');
@@ -43,21 +53,29 @@ export default class Room {
       break;
     case 'PLAYING':
       this.state = 'playing';
+      this[kPlayerTime] = message.currentTime;
+      this[kServerTime] = Date.now();
       break;
     case 'PAUSE':
       this.state = 'paused';
+      this[kPlayerTime] = message.currentTime;
+      this[kServerTime] = undefined;
       break;
     default:
       break;
     }
-    for (const socket of this.clients) {
+    for (const socket of this[kClients]) {
       if (socket === fromSocket) continue;
       socket.send(JSON.stringify(message));
     }
   }
 
   toJSON() {
-    return { ...this, clients: undefined };
+    return {
+      ...this,
+      users: this.users,
+      currentTime: this.currentTime,
+    };
   }
 }
 Room.ALLOWED_MESSAGES = [
